ff86767d514016f6b8d82e7373810e4876e1cd95
andrewsys <ags.andrew1@gmail.com>
2024-09-18 22:59:06 -0300

    part2

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -6,12 +6,4 @@ const getAll = () => {
     return request.then(response => response.data)
 }
 
-<<<<<<< HEAD
-=======
-const getWeather = ({capital, country}) => {
-    const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital},${country}&appid=04a13a95cd2135aa78a4416332515ca9`)
-    return request.then(response => response.data)
-}
-
->>>>>>> 272e8b555fa3608d642c67b8129ed5db450e596f
 export default { getAll, getWeather }
